b58cdcc2116ceb46adb1a1bbf2a6a930957a66a6
the.pythy <devnull@localhost>
2006-10-19 07:00:02 +0000

    Cleanups in Django/TG examples, thanks to I.Sagalaev

--- doc/examples-django/pytilsex/settings.py
+++ doc/examples-django/pytilsex/settings.py
@@ -14,8 +14,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = 'pytilsex.db'             # Or path to database file if using sqlite3.
+DATABASE_ENGINE = ''           # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = ''             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -57,8 +57,6 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.doc.XViewMiddleware',
 )
 
@@ -70,11 +68,9 @@ TEMPLATE_DIRS = (
     CURRPATH+'/templates',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = []
+
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
 # -- install pytils  
     'pytils',
 )
