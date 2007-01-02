d7a63930e4ca68051a100e8e9ffa819e39407f02
the.pythy <devnull@localhost>
2007-01-02 09:22:01 +0000

    Add GNU GPL2 notice in each source file (for the request of Mikhail Gusarov)

--- pytils/dt.py
+++ pytils/dt.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 # -*- test-case-name: pytils.test.test_dt -*-
-# License: GNU GPL2
-# Author: Pythy <the.pythy@gmail.com>
+# PyTils - simple processing for russian strings
+# Copyright (C) 2006-2007  Yury Yurevich
+#
+# http://gorod-omsk.ru/blog/pythy/projects/pytils/
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 """
 Russian dates without locales
 """
