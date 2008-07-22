4977ff45eb0e723d7f9e633ba757c8f92c8a9499
pythy <the.pythy@gmail.com>
2008-07-22 11:30:49 +0700

    Improve style of unit tests for typo

--- pytils/test/test_typo.py
+++ pytils/test/test_typo.py
@@ -18,7 +18,7 @@ Unit-tests for pytils.typo
 """
 
 import unittest
-import pytils
+from pytils import typo
 
 def cb_testrule(x):
     return x
@@ -31,77 +31,153 @@ class HelpersTestCase(unittest.TestCase):
         """
         unit-test for pytils.typo._get_rule_by_name
         """
-        self.assert_(callable(pytils.typo._get_rule_by_name('testrule')))
-        self.assertEquals(pytils.typo._get_rule_by_name('testrule').__name__, 'rl_testrule')
+        self.assert_(
+            callable(
+                typo._get_rule_by_name('testrule')
+        ))
+        self.assertEquals(
+            'rl_testrule',
+            typo._get_rule_by_name('testrule').__name__
+        )
     
     def testResolveRule(self):
         """
         unit-test for pytils.typo._resolve_rule
         """
-        self.assert_(callable(pytils.typo._resolve_rule_name('testrule')[1]))
-        self.assert_(callable(pytils.typo._resolve_rule_name(cb_testrule)[1]))
-        self.assertEquals('testrule', pytils.typo._resolve_rule_name('testrule')[0])
-        self.assertEquals('cb_testrule', pytils.typo._resolve_rule_name(cb_testrule)[0])
+        self.assert_(
+            callable(
+                typo._resolve_rule_name('testrule')[1]
+        ))
+        self.assert_(
+            callable(
+                typo._resolve_rule_name(cb_testrule)[1]
+        ))
+        self.assertEquals(
+            'testrule',
+            typo._resolve_rule_name('testrule')[0]
+        )
+        self.assertEquals(
+            'cb_testrule',
+            typo._resolve_rule_name(cb_testrule)[0]
+        )
 
     def testResolveRuleWithForcedName(self):
         """
         unit-test for pytils.typo._resolve_rule with forced_name arg
         """
-        self.assert_(callable(pytils.typo._resolve_rule_name('testrule', 'newrule')[1]))
-        self.assert_(callable(pytils.typo._resolve_rule_name(cb_testrule, 'newrule')[1]))
-        self.assertEquals('newrule', pytils.typo._resolve_rule_name('testrule', 'newrule')[0])
-        self.assertEquals('newrule', pytils.typo._resolve_rule_name(cb_testrule, 'newrule')[0])
+        self.assert_(
+            callable(typo._resolve_rule_name('testrule', 'newrule')[1]
+        ))
+        self.assert_(
+            callable(typo._resolve_rule_name(cb_testrule, 'newrule')[1]
+        ))
+        self.assertEquals(
+            'newrule', 
+            typo._resolve_rule_name('testrule', 'newrule')[0]
+        )
+        self.assertEquals(
+            'newrule',
+            typo._resolve_rule_name(cb_testrule, 'newrule')[0]
+        )
 
 class TypographyApplierTestCase(unittest.TestCase):
     """
     Test case for typography rule applier pytils.typo.Typography
     """
     def testExpandEmptyArgs(self):
-        self.assertEquals({}, 
-            pytils.typo.Typography().rules)
-        self.assertEquals([], 
-            pytils.typo.Typography().rules_names)
+        self.assertEquals(
+            {},
+            typo.Typography().rules
+        )
+        self.assertEquals(
+            [],
+            typo.Typography().rules_names
+        )
     
     def testExpandSimpleStrArgs(self):
-        self.assertEquals({'testrule': pytils.typo.rl_testrule}, 
-            pytils.typo.Typography('testrule').rules)
-        self.assertEquals(['testrule'], 
-            pytils.typo.Typography('testrule').rules_names)
+        self.assertEquals(
+            {'testrule': typo.rl_testrule}, 
+            typo.Typography('testrule').rules
+        )
+        self.assertEquals(
+            ['testrule'],
+            typo.Typography('testrule').rules_names
+        )
     
     def testExpandDictStrArgs(self):
-        self.assertEquals({'testrule': pytils.typo.rl_testrule, 'newrule': pytils.typo.rl_testrule}, 
-            pytils.typo.Typography('testrule', {'newrule': 'testrule'}).rules)
-        self.assertEquals(['testrule', 'newrule'], 
-            pytils.typo.Typography('testrule', {'newrule': 'testrule'}).rules_names)
+        self.assertEquals(
+            {
+                'testrule': typo.rl_testrule, 
+                'newrule':  typo.rl_testrule
+            },
+            typo.Typography('testrule', {'newrule': 'testrule'}).rules
+        )
+        self.assertEquals(
+            ['testrule', 'newrule'],
+            typo.Typography('testrule', {'newrule': 'testrule'}).rules_names
+        )
 
     def testExpandSimpleCallableArgs(self):
-        self.assertEquals({'cb_testrule': cb_testrule}, 
-            pytils.typo.Typography(cb_testrule).rules)
-        self.assertEquals(['cb_testrule'], 
-            pytils.typo.Typography(cb_testrule).rules_names)
+        self.assertEquals(
+            {'cb_testrule': cb_testrule},
+            typo.Typography(cb_testrule).rules
+        )
+        self.assertEquals(
+            ['cb_testrule'],
+            typo.Typography(cb_testrule).rules_names
+        )
     
     def testExpandDictCallableArgs(self):
-        self.assertEquals({'cb_testrule': cb_testrule, 'newrule': cb_testrule}, 
-            pytils.typo.Typography(cb_testrule, {'newrule': cb_testrule}).rules)
-        self.assertEquals(['cb_testrule', 'newrule'], 
-            pytils.typo.Typography(cb_testrule, {'newrule': cb_testrule}).rules_names)
+        self.assertEquals(
+            {
+                'cb_testrule': cb_testrule,
+                'newrule': cb_testrule
+            }, 
+            typo.Typography(cb_testrule, {'newrule': cb_testrule}).rules
+        )
+        self.assertEquals(
+            ['cb_testrule', 'newrule'],
+            typo.Typography(cb_testrule, {'newrule': cb_testrule}).rules_names
+        )
 
     def testExpandMixedArgs(self):
-        self.assertEquals({'cb_testrule': cb_testrule, 'newrule': pytils.typo.rl_testrule},
-            pytils.typo.Typography(cb_testrule, newrule='testrule').rules)
-        self.assertEquals(['cb_testrule', 'newrule'],
-            pytils.typo.Typography(cb_testrule, newrule='testrule').rules_names)
-        self.assertEquals({'cb_testrule': cb_testrule, 'testrule': pytils.typo.rl_testrule},
-            pytils.typo.Typography(cb_testrule, 'testrule').rules)
-        self.assertEquals(['cb_testrule', 'testrule'],
-            pytils.typo.Typography(cb_testrule, 'testrule').rules_names)
+        self.assertEquals(
+            {
+                'cb_testrule': cb_testrule,
+                'newrule': typo.rl_testrule
+            },
+            typo.Typography(cb_testrule, newrule='testrule').rules
+        )
+        self.assertEquals(
+            ['cb_testrule', 'newrule'],
+            typo.Typography(cb_testrule, newrule='testrule').rules_names
+        )
+        self.assertEquals(
+            {
+                'cb_testrule': cb_testrule,
+                'testrule': typo.rl_testrule
+            },
+            typo.Typography(cb_testrule, 'testrule').rules
+        )
+        self.assertEquals(
+            ['cb_testrule', 'testrule'],
+            typo.Typography(cb_testrule, 'testrule').rules_names
+        )
 
     def testRecommendedArgsStyle(self):
         lambdarule = lambda x: x
-        self.assertEquals({'cb_testrule': cb_testrule, 'testrule': pytils.typo.rl_testrule, 'newrule': lambdarule},
-            pytils.typo.Typography([cb_testrule], ['testrule'], {'newrule': lambdarule}).rules)
-        self.assertEquals(['cb_testrule', 'testrule', 'newrule'],
-            pytils.typo.Typography([cb_testrule], ['testrule'], {'newrule': lambdarule}).rules_names)
+        self.assertEquals(
+            {
+                'cb_testrule': cb_testrule,
+                'testrule': typo.rl_testrule,
+                'newrule': lambdarule
+            },
+            typo.Typography([cb_testrule], ['testrule'], {'newrule': lambdarule}).rules
+        )
+        self.assertEquals(
+            ['cb_testrule', 'testrule', 'newrule'],
+            typo.Typography([cb_testrule], ['testrule'], {'newrule': lambdarule}).rules_names
+        )
 
 if __name__ == '__main__':
     unittest.main()
