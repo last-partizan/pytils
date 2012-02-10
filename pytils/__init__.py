e3fa928224e68ef5802e65bded96abe6de9a600d
Anton Patrushev <apatrushev@gmail.com>
2012-02-10 16:12:19 +0600

    some more future import fix

--- pytils/__init__.py
+++ pytils/__init__.py
@@ -17,7 +17,7 @@
 """
 Simple processing for russian strings
 """
+from __future__ import print_function, absolute_import, division, unicode_literals
 VERSION = '0.2.4dev'
 
-from __future__ import print_function, absolute_import, division, unicode_literals
 from pytils import numeral, dt, translit, typo, err
