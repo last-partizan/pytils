8721bdc4e9f3a67a62ec98194f1d11548021835a
Yury Yurevich <yyurevich@odeskps.com>
2012-02-12 01:11:09 +0700

    Change license to MIT.

--- pytils/test/test_typo.py
+++ pytils/test/test_typo.py
@@ -1,18 +1,4 @@
 # -*- coding: utf-8 -*-
-# pytils - russian-specific string utils
-# Copyright (C) 2006-2009  Yury Yurevich
-#
-# http://pyobject.ru/projects/pytils/
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation, version 2
-# of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
 """
 Unit-tests for pytils.typo
 """
